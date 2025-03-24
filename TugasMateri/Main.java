945250f73ebfdec080ffb1b500849846d6a075b3
KayisHlm <kayishilmi24@gmail.com>
2025-03-25 01:01:35 +0700

    Add files via upload

---
+++ TugasMateri/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        BangunDatar B1  = new BangunDatar();
+        BangunDatar P1 = new Persegi(10,"","");
+        Persegi P2 = new Persegi(5,"","");
+        BangunDatar L1 = new Lingkaran(7,"","");
+        Lingkaran L1 = new Lingkaran(14,"","");
+    }
+}
