2a1fd49b7c5b22ad6d14fbd499cb32b8eacdb0a9
KayisHlm <kayishilmi24@gmail.com>
2025-03-25 01:02:06 +0700

    Update BangunDatar.java

--- TugasMateri/BangunDatar.java
+++ TugasMateri/BangunDatar.java
@@ -53,6 +53,27 @@ public abstract class BangunDatar {
 
 }
 
+
+
+/* 
+ *   Soal 1
+ *   Apakah method yang dibuat pada langkah nomor 4 dapat digunakan untuk membandingkan objek
+ *   bangun datar yang berbeda?
+ *   Jawab: Bisa, karena method isEqualLuas() merupakan method yang dibuat pada superclass BangunDatar
+ *   yang dapat digunakan untuk membandingkan objek bangun datar yang berbeda.
+ *
+ *   Soal 2
+ *   Jika BangunDatar tidak dijadikan sebagai abstract class, dapatkah Anda membuat method isEqualLuas()
+ *   dan isEqualKeliling pada class BangunDatar? Mengapa?
+ *   Jawab: Tidak bisa, karena method isEqualLuas() dan isEqualKeliling() harus diimplementasikan pada subclass
+ *   karena setiap bangun datar memiliki rumus luas dan keliling yang berbeda-beda. Sehingga, BangunDatar harus
+ *   dijadikan sebagai abstract class agar method-method tersebut dapat diimplementasikan pada subclass.
+ *
+ *   Soal 3
+ *   Apakah kelebihan saat class BangunDatar dijadikan sebagai abstract class daripada non-abstract class?
+ *   Jawab: Banyak kelebihan yaitu memungkinkan method abstract untuk implementasi yang spesifik pada tiap subclassnya, mencegah instansiasi
+ *   BangunDatar karena merupakan suatu konsep(template), dan supaya subclass konsisten dalam implementasi method yang diwarisi dari superclass.
+ */
 /*
  * Jawaban soal
  * 1. keuntungan saat method zoomIn(), zoomOut(), dan zoom() dikemas
@@ -61,3 +82,4 @@ public abstract class BangunDatar {
  * dalam abstract class BangunDatar. Hal ini membuat kodingan menjadi lebih efektif
  * 
  */
+
